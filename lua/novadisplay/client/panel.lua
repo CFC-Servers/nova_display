8b79ad40c8ca18d4bf3613b82fe70125dd64c531
Periapsises <yannknoell@gmail.com>
2022-01-07 16:49:01 -0500

    Wait for custom vgui to be loaded

--- lua/novadisplay/client/panel.lua
+++ lua/novadisplay/client/panel.lua
@@ -1,8 +1,15 @@
 local scoreboardPanel = {}
 
 function scoreboardPanel:Init()
+    local w, h = ScrW() * 0.7, ScrH() * 0.7
 
+    self:SetSize( w, h )
+    self:Center()
+
+    self:MakePopup()
+    self:SetKeyboardInputEnabled( false )
+
+    self:Hide()
 end
 
-vgui.Register( "nova_display", scoreboardPanel, "DPanel" )
-NovaDisplay.panel = vgui.Create( "nova_display" )
+vgui.Register( "NovaDisplay_Scoreboard", scoreboardPanel, "DPanel" )
