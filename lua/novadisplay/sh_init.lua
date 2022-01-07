691687c21411577ace8ede60fbf0e16561e6ebbb
Periapsises <yannknoell@gmail.com>
2022-01-07 17:58:12 -0500

    Include files from client realm

--- lua/novadisplay/sh_init.lua
+++ lua/novadisplay/sh_init.lua
@@ -1,21 +1,24 @@
 AddCSLuaFile( "sh_config.lua" )
+AddCSLuaFile( "client/basepanel.lua" )
 AddCSLuaFile( "client/novadisplay.lua" )
-AddCSLuaFile( "client/panel.lua" )
+AddCSLuaFile( "client/scoreboard.lua" )
 
 include( "sh_config.lua" )
-
-local files = file.Find( "novadisplay/client/panels/*.lua", "LUA" )
-
-for _, fileName in ipairs( files ) do
-    local filePath = "novadisplay/client/panels/" .. fileName
-
-    AddCSLuaFile( filePath )
-    include( filePath )
-end
 
 if SERVER then
     -- include( "server_file" )
 else
+    include( "client/basepanel.lua" )
+
+    local files = file.Find( "novadisplay/client/panels/*.lua", "LUA" )
+
+    for _, fileName in ipairs( files ) do
+        local filePath = "novadisplay/client/panels/" .. fileName
+
+        AddCSLuaFile( filePath )
+        include( filePath )
+    end
+
     include( "client/novadisplay.lua" )
-    include( "client/panel.lua" )
+    include( "client/scoreboard.lua" )
 end
