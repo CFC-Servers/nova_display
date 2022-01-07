8cee5a55162cf1e3abce3c33a13e6b76fb59278c
Periapsises <yannknoell@gmail.com>
2022-01-07 17:26:24 -0500

    Include all files from specific folder

--- lua/novadisplay/sh_init.lua
+++ lua/novadisplay/sh_init.lua
@@ -4,6 +4,15 @@ AddCSLuaFile( "client/panel.lua" )
 
 include( "sh_config.lua" )
 
+local files = file.Find( "novadisplay/client/panels/*.lua", "LUA" )
+
+for _, fileName in ipairs( files ) do
+    local filePath = "novadisplay/client/panels/" .. fileName
+
+    AddCSLuaFile( filePath )
+    include( filePath )
+end
+
 if SERVER then
     -- include( "server_file" )
 else
